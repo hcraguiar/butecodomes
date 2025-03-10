88d3d0c90f7ee6139dacc2b48a596bf3112cff0a
Helder Aguiar <hcraguiar@gmail.com>
2025-03-10 09:46:11 -0300

    First commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,35 +1,48 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
-  DocumentDuplicateIcon,
+  StarIcon,
+  CalendarIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    name: 'Avaliações',
+    href: '/dashboard/reviews',
+    icon: StarIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Membros', href: '/dashboard/members', icon: UserGroupIcon },
+  { name: 'Proximos butecos', href: '/dashboard/suggestions', icon: CalendarIcon },
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-amber-300 hover:text-amber-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                'bg-amber-300 text-amber-600': pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
