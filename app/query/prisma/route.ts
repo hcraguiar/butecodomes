917291bc310ba18f8239583c9a3badeca49b4614
Helder Aguiar <hcraguiar@gmail.com>
2025-04-02 15:51:56 -0300

    FIX: Resolve merge conflict

--- app/query/prisma/route.ts
+++ app/query/prisma/route.ts
@@ -1,14 +1,31 @@
+<<<<<<< HEAD:app/query/prisma/route.ts
 import { prisma } from "@/prisma/prisma";
+=======
+// import postgres from 'postgres';
 
-async function listUsers() {
-  return prisma.user.findMany();
-}
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+>>>>>>> main:app/query/route.ts
+
+// async function listInvoices() {
+// 	const data = await sql`
+//     SELECT invoices.amount, customers.name
+//     FROM invoices
+//     JOIN customers ON invoices.customer_id = customers.id
+//     WHERE invoices.amount = 666;
+//   `;
+
+// 	return data;
+// }
 
 export async function GET() {
-  try {
-  	return Response.json(await listUsers());
-  } catch (error) {
-  	return Response.json({ error }, { status: 500 });
-  }
+  return Response.json({
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
 }
 
