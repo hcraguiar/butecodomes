743a96b4f1fa0c4c7d9fa233956274cfc43017ff
Helder Aguiar <hcraguiar@gmail.com>
2025-04-07 21:04:21 -0300

    FIX: Mudanças na configuração do Google Auth

--- auth.ts
+++ auth.ts
@@ -45,21 +45,15 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          scope: "openid email profile",
+        }
+      }
     })
   ],
   callbacks: {
-    // authorized({ auth, request: { nextUrl } }) {
-    //   const isLoggedIn = !!auth?.user;
-    //   const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-    //   if (isOnDashboard) {
-    //     if (isLoggedIn) return true;
-    //     return !!auth;
-    //   } else if (isLoggedIn) {
-    //     return Response.redirect(new URL('/dashboard', nextUrl));
-    //   }
-    //   return true;
-    // },
     async signIn({ user, account }) {
       if (account?.provider === 'google' && user?.email) {
         const client = await neon.connect();
